0dc240ac5b3daae52f3b49a755b7014147ced935
AIKONS2024 <aikong2024@gmail.com>
2024-01-13 21:40:26 +0900

    - fixed file name, sigmoid

--- keras/keras14_overfit1_boston.py
+++ keras/keras14_overfit1_boston.py
@@ -30,7 +30,7 @@ model.add(Dense(1))
 # 컴파일, 훈련
 model.compile(loss="mse", optimizer="adam")
 start_time = time.time()
-hist = model.fit(x_train, y_train, epochs=10, batch_size=16, validation_split=0.2)
+history = model.fit(x_train, y_train, epochs=10, batch_size=16, validation_split=0.2)
 end_time = time.time()
 
 # 평가, 예측
