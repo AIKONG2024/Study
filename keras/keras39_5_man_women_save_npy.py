7891447c40c69c237b03c22a0039b7bfff601804
AIKONG2024 <aikong2024@gmail.com>
2024-01-24 21:11:24 +0900

    -add man_women

--- keras/keras39_5_man_women_save_npy.py
+++ keras/keras39_5_man_women_save_npy.py
@@ -17,8 +17,8 @@ path_test ='c:/_data/image/man_women/test/'
 
 xy_train = xy_traingen.flow_from_directory(
     path_train,
-    batch_size=20000,
-    target_size=(100,100),
+    batch_size=3309,
+    target_size=(200,200),
     class_mode='binary',
     shuffle=True
 )
@@ -26,7 +26,7 @@ xy_train = xy_traingen.flow_from_directory(
 xy_test = xy_testgen.flow_from_directory(
     path_test,
     batch_size=20000,
-    target_size=(100,100),
+    target_size=(200,200),
     class_mode='binary'
 )
 print(xy_test[0][1].shape)
@@ -34,7 +34,8 @@ unique, count =  np.unique(xy_test[0][1] , return_counts=True)
 print(unique, count)
 
 np_path = '../_data/_save_npy/'
-np.save(np_path + 'keras39_3_x_train.npy', arr=xy_train[0][0])
-np.save(np_path + 'keras39_3_y_train.npy', arr=xy_train[0][1])
-np.save(np_path + 'keras39_3_x_test.npy', arr=xy_test[0][0])
+np.save(np_path + 'keras39_5_x_train.npy', arr=xy_train[0][0])
+np.save(np_path + 'keras39_5_y_train.npy', arr=xy_train[0][1])
+np.save(np_path + 'keras39_5_x_test.npy', arr=xy_test[0][0])
+np.save(np_path + 'keras39_5_y_test.npy', arr=xy_test[0][1])
 
