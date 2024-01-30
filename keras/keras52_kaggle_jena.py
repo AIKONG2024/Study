a0b32ce1487fcc29da1e0115f90c1c546ad914ba
AIKONG2024 <aikong2024@gmail.com>
2024-01-30 21:41:45 +0900

    -fix kaggle jena

--- keras/keras52_kaggle_jena.py
+++ keras/keras52_kaggle_jena.py
@@ -2,16 +2,18 @@
 import numpy as np
 import pandas as pd
 from keras.models import Sequential
-from keras.layers import Dense, SimpleRNN, LSTM, GRU
+from keras.layers import Dense, SimpleRNN, LSTM, GRU, Bidirectional
 import time
-time_steps = 20
-def split_xy(dataFrame, size):
+from keras.callbacks import EarlyStopping
+
+time_steps = 1
+def split_xy(dataFrame, size, y_column):
     split_start_time = time.time()
     xs = []
     ys = [] 
     for i in range(len(dataFrame) - size ):
         x = dataFrame[i : (i + size)]
-        y = dataFrame[i : (i + size)]['T (degC)']
+        y = dataFrame[i : (i + size)][y_column]
         xs.append(x)
         ys.append(y)
     split_end_time = time.time()
@@ -29,23 +31,39 @@ print(jena_csv.shape)  # (420551, 14)
 #결측치 확인
 # print(np.unique(jena_csv.isna().sum()))
 
-x, y = split_xy(jena_csv, time_steps)  # spliting time :  5.13
+x, y = split_xy(jena_csv, time_steps, 'T (degC)')  # spliting time :  5.13
 print(x.shape, y.shape)
 
 # 2. 모델 구성
 model = Sequential()
-model.add(LSTM(16, input_shape=(time_steps,14)))
-model.add(Dense(64))
-model.add(Dense(32))
-model.add(Dense(16))
-model.add(Dense(8))
+model.add(LSTM(64, input_shape=(time_steps,14)))
+model.add(Dense(64,activation='relu'))
+model.add(Dense(64,activation='relu'))
+model.add(Dense(32,activation='relu'))
+model.add(Dense(32,activation='relu'))
 model.add(Dense(1))
 
 # 3.컴파일, 훈련
 model.compile(loss='mse', optimizer='adam')
-model.fit(x,y, epochs=100, batch_size=1000)
+model.fit(x,y, epochs=2000, batch_size=5000, validation_split=0.1, callbacks=[
+    EarlyStopping(monitor='val_loss', mode = 'min', patience=100,restore_best_weights=True)
+])
 
 # 4. 평가, 예측
 loss = model.evaluate(x,y)
 print("loss : ", loss)
 print("predict : \n" , model.predict(x))
+
+'''
+loss :  0.004464844241738319
+13143/13143 [==============================] - 10s 747us/step
+predict : 
+ [[-8.010506 ]
+ [-8.392689 ]
+ [-8.482526 ]
+ ...
+ [-3.3230095]
+ [-3.1502385]
+ [-4.2950053]]
+
+'''
