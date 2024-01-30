50a729c3cb30d820de0f1f83debd2f1e012f798f
AIKONG2024 <aikong2024@gmail.com>
2024-01-30 18:30:58 +0900

    -fix jena

--- keras/keras52_kaggle_jena.py
+++ keras/keras52_kaggle_jena.py
@@ -4,44 +4,37 @@ import pandas as pd
 from keras.models import Sequential
 from keras.layers import Dense, SimpleRNN, LSTM, GRU
 import time
-
 time_steps = 20
-def split_x(dataFrame, size):
+def split_xy(dataFrame, size):
     split_start_time = time.time()
-    aaa = []
-    for i in range(len(dataFrame) - size + 1):
-        subset = dataFrame[i : (i + size)]
-        aaa.append(subset)
+    xs = []
+    ys = [] 
+    for i in range(len(dataFrame) - size ):
+        x = dataFrame[i : (i + size)]
+        y = dataFrame[i : (i + size)]['T (degC)']
+        xs.append(x)
+        ys.append(y)
     split_end_time = time.time()
-    print("time : ", np.round(split_end_time - split_start_time, 2),  "sec")
-    return np.array(aaa)
+    print("spliting time : ", np.round(split_end_time - split_start_time, 2),  "sec")
+    return (np.array(xs), np.array(ys))
 
 # 1. 데이터
 path = "C:\_data\kaggle\jena\\"
 jena_csv = pd.read_csv(path + "jena_climate_2009_2016.csv", index_col="Date Time")
 print(jena_csv.shape)  # (420551, 14)
 
-#T (degC) 맨뒤로 보냄
-temp = jena_csv['T (degC)']
-dataFrame = jena_csv.drop(columns=['T (degC)'])
-dataFrame['T (degC)'] = temp
 #확인
 # print(dataFrame.head(1))
 
 #결측치 확인
 # print(np.unique(jena_csv.isna().sum()))
 
-splited_csv = split_x(jena_csv, time_steps)  # spliting time :  5.13
-print(splited_csv.shape)  # (420551, 14)
-
-x = splited_csv[:,:-1]
-y = splited_csv[:,-1]
-
-print(x.shape, y.shape)  # (420532, 19, 14) (420532, 14)
+x, y = split_xy(jena_csv, time_steps)  # spliting time :  5.13
+print(x.shape, y.shape)
 
 # 2. 모델 구성
 model = Sequential()
-model.add(SimpleRNN(16, input_shape=(19,14)))
+model.add(LSTM(16, input_shape=(time_steps,14)))
 model.add(Dense(64))
 model.add(Dense(32))
 model.add(Dense(16))
@@ -49,9 +42,10 @@ model.add(Dense(8))
 model.add(Dense(1))
 
 # 3.컴파일, 훈련
-model.compile(loss='mse', optimizer='adam', metrics=['mae'])
-model.fit(x,y, epochs=100, batch_size=500)
+model.compile(loss='mse', optimizer='adam')
+model.fit(x,y, epochs=100, batch_size=1000)
 
 # 4. 평가, 예측
-model.evaluate(x,y)
+loss = model.evaluate(x,y)
+print("loss : ", loss)
 print("predict : \n" , model.predict(x))
