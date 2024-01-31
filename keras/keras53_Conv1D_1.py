7398db624557ea459281832e8332510fa64990ae
AIKONG2024 <aikong2024@gmail.com>
2024-01-31 12:07:47 +0900

    -add Conv1d

---
+++ keras/keras53_Conv1D_1.py
@@ -0,0 +1,61 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, SimpleRNN, Dropout, LSTM, Conv1D, Flatten
+from keras.callbacks import EarlyStopping
+
+# 1. 데이터
+datasets = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+# timesteps :3
+x = np.array(
+    [
+        [1, 2, 3],
+        [2, 3, 4],
+        [3, 4, 5],
+        [4, 5, 6],
+        [5, 6, 7],
+        [6, 7, 8],
+        [7, 8, 9],
+    ]
+)
+
+y = np.array([4, 5, 6, 7, 8, 9, 10])
+
+print(x.shape, y.shape) #(7, 3) (7,)
+x = x.reshape(7, 3, 1)
+print(x.shape, y.shape) #(7, 3, 1) (7,)
+
+#2. 모델구성
+model = Sequential()
+model.add(Conv1D(filters=10, kernel_size=(2,), input_shape = (3,1)))
+model.add(Flatten())
+model.add(Dense(7, activation='relu'))
+model.add(Dense(1))
+
+model.summary()
+
+'''
+LSTM
+_________________________________________________________________
+Total params: 565
+_________________________________________________________________
+
+Conv1D
+Total params: 185
+'''
+
+#3. 컴파일, 훈련
+model.compile(loss='mse', optimizer='adam', metrics=["mae"])
+model.fit(x, y, epochs=2000, batch_size=4,validation_split=0.2, callbacks=[
+    EarlyStopping(monitor='val_loss', mode='min', patience=1000, restore_best_weights=True)
+])
+
+# 4. 평가 예측
+result = model.evaluate(x,y)
+print('loss mse: ', result[0])
+print('loss mae: ', result[1])
+#(3,) -> (1,3,1)
+y_pred = np.array([8,9,10]).reshape(1,3,1)
+y_pred = model.predict(y_pred)
+print("predict : ", y_pred)
+#predict :  [[11.000721]]
---
+++ keras/keras53_Conv1D_2_scale.py
@@ -0,0 +1,57 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+from keras.callbacks import EarlyStopping
+
+#80 예측
+#1. 데이터
+x = np.array([[1,2,3],[2,3,4],[3,4,5],[4,5,6],
+           [5,6,7],[6,7,8],[7,8,9],[8,9,10],
+           [9,10,11],[10,11,12],
+           [20,30,40],[30,40,50],[40,50,60]])
+y = np.array([4,5,6,7,8,9,10,11,12,13,50,60,70])
+predict = np.array([50,60,70])
+
+print(x.shape, y.shape)#(13, 3)(3,)
+x = x.reshape(13,3,1)
+#Conv1D로 구성
+
+#2. 모델구성
+model = Sequential()
+model.add(LSTM(70, input_shape = (3,1),))
+model.add(Dense(64))
+model.add(Dense(128))
+model.add(Dense(256,activation='relu'))
+model.add(Dense(512))
+model.add(Dense(1))
+
+#3. 모델 훈련, 컴파일
+model.compile(loss='mse', optimizer='adam', metrics=['mae'])
+hist = model.fit(x,y,epochs=10000,batch_size=100, callbacks=[
+    EarlyStopping(monitor='loss', mode='min', patience=500, restore_best_weights=True)
+])
+
+# 평가, 예측
+loss = model.evaluate(x,y)
+y_predict = model.predict(predict.reshape(1,3,1))
+
+print("mse : ", loss[0])
+print("mae : ", loss[1])
+print("predict :", y_predict)
+
+
+import matplotlib.pyplot as plt
+plt.figure(figsize=(9,6))
+plt.plot(hist.history['loss'], c ='red', label = 'loss', marker = '.')
+plt.xlabel = 'epoch'
+plt.ylabel = 'loss'
+plt.grid()
+plt.show()
+
+
+
+'''
+mse :  1.352716026303824e-05
+mae :  0.0026057499926537275
+predict : [[78.37969]]
+'''
