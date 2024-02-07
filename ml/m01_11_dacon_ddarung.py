98e6623b24ec3bccb8a5b9ccf1b6443b24faf359
AIKONG2024 <aikong2024@gmail.com>
2024-02-07 12:57:10 +0900

    -fix ml01

--- ml/m01_11_dacon_ddarung.py
+++ ml/m01_11_dacon_ddarung.py
@@ -7,37 +7,25 @@ from keras.layers import Dense, Dropout, Conv2D, Flatten, LSTM, Conv1D
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 
-#1. 데이터 - 경로데이터를 메모리에 땡겨옴
+#1. 데이터
 path = "c:/_data/dacon/ddarung/"
-train_csv = pd.read_csv(path + "train.csv", index_col=0) #c:/_data/dacon/ddarung/train.csv
-#문자열에서 경로를 찾아줄때 예약어가 있으면 \를 한번 더 붙여줌.
-#경로 찾는것 \ ,\\, /, // 다 됨.
-#가독성 면에서 슬래시 모양은 하나로 통일.
-#default 맨 윗줄을 컬럼명으로 인식.(대부분 컬럼명이 들어감으로)
-
+train_csv = pd.read_csv(path + "train.csv", index_col=0) 
 test_csv = pd.read_csv(path + "test.csv", index_col=0) 
 submission_csv = pd.read_csv(path + "submission.csv")
 train_csv = train_csv.fillna(test_csv.mean()) # 715 non-null
 test_csv = test_csv.fillna(test_csv.mean()) # 715 non-null
 
-######### x와 y를 분리 ###########
 x = train_csv.drop(['count'], axis=1) #axis 0이 행 1이 열
-# print(x)
-
 y = train_csv['count'] 
-# print(y)
 
 x_train, x_test, y_train, y_test = train_test_split(x,y, shuffle=True, train_size=0.7, random_state=12345)
 
-from sklearn.preprocessing import MinMaxScaler, MaxAbsScaler, StandardScaler, RobustScaler
+from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 scaler.fit(x_train)
 x_train = scaler.transform(x_train)
 x_test = scaler.transform(x_test)
 test_csv = scaler.transform(test_csv)
-
-print(x_train.shape) #(1021, 9)
-
 #Early Stopping
 
 #2. 모델
@@ -48,11 +36,14 @@ model = LinearSVR()
 model.fit(x_train, y_train) 
 
 #4. 평가, 예측
+from sklearn.metrics import mean_squared_error
 r2 = model.score(x_test, y_test)
 y_predict = model.predict(x_test)
 r2_pred = r2_score(y_test, y_predict)
+mse_loss = mean_squared_error(y_test, y_predict)
 print("r2 : ", r2)
 print("eval_r2 : ", r2_pred)
+print("mse loss :", mse_loss)
 
 y_submit = model.predict(test_csv) # count 값이 예측됨.
 submission_csv['count'] = y_submit
@@ -65,7 +56,7 @@ file_path = path + f"submission_{save_time}.csv"
 submission_csv.to_csv(file_path, index=False)
 
 '''
-기존 : 
+mse loss : 3237.6334716869906
 r2 :  0.5350276904530479
 eval_r2 :  0.5350276904530479
 '''
