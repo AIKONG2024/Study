83272019543f0fe850d1941d1538b5f820499d53
AIKONG2024 <aikong2024@gmail.com>
2024-02-14 16:10:18 +0900

    -fix FI delete

--- ml/m25_FI_05_fetch_covtype.py
+++ ml/m25_FI_05_fetch_covtype.py
@@ -18,14 +18,24 @@ y = datasets.target
 # x = np.delete(x, 0, axis=1)
 # 판다스로 삭제
 x = pd.DataFrame(data=x, columns=datasets.feature_names)
-# x = x.drop(
-#     [
-#         "nonflavanoid_phenols",
-#         "magnesium",
-#         "proanthocyanins"
-#     ],
-#     axis=1,
-# )
+x = x.drop(
+    [
+        "Soil_Type_14",
+        "Soil_Type_6",
+        "Soil_Type_7",
+        "Soil_Type_35",
+        "Soil_Type_8",
+        "Soil_Type_24",
+        "Soil_Type_27",
+        "Soil_Type_17",
+        "Soil_Type_25",
+        "Soil_Type_13",
+        "Soil_Type_4",
+        "Soil_Type_36",
+        "Soil_Type_33",
+    ],
+    axis=1,
+)
 
 
 from sklearn.preprocessing import LabelEncoder
@@ -61,24 +71,22 @@ for model in models:
     # print(type(model).__name__ ,":", model.feature_importances_)
     
     # 25%이하 컬럼
-    feature_importance_set = pd.DataFrame({'feature': x.columns, 'importance':model.feature_importances_})
-    feature_importance_set.sort_values('importance', inplace=True)
-    delete_0_25_features = feature_importance_set['feature'][:int(len(feature_importance_set.values) * 0.25)]
-    delete_0_25_importance = feature_importance_set['importance'][:int(len(feature_importance_set.values) * 0.25)]
-    print(f'''
-    제거 할 컬럼명 : 
-    {delete_0_25_features}  
-    제거 할 feature_importances_ : 
-    {delete_0_25_importance}    
-    ''')
+    # feature_importance_set = pd.DataFrame({'feature': x.columns, 'importance':model.feature_importances_})
+    # feature_importance_set.sort_values('importance', inplace=True)
+    # delete_0_25_features = feature_importance_set['feature'][:int(len(feature_importance_set.values) * 0.25)]
+    # delete_0_25_importance = feature_importance_set['importance'][:int(len(feature_importance_set.values) * 0.25)]
+    # print(f'''
+    # 제거 할 컬럼명 : 
+    # {delete_0_25_features}  
+    # 제거 할 feature_importances_ : 
+    # {delete_0_25_importance}    
+    # ''')
 
 '''
-[LogisticRegression] model acc :  0.7229495593904902
-[LogisticRegression] eval_acc :  0.7229495593904902
-[KNeighborsClassifier] model acc :  0.9258249954103176
-[KNeighborsClassifier] eval_acc :  0.9258249954103176
-[DecisionTreeClassifier] model acc :  0.9352281072149807
-[DecisionTreeClassifier] eval_acc :  0.9352281072149807
 [RandomForestClassifier] model acc :  0.9532081879933909
 [RandomForestClassifier] eval_acc :  0.9532081879933909
+=================
+제거후
+[RandomForestClassifier] model acc :  0.9542982375619607
+[RandomForestClassifier] eval_acc :  0.012541307141545804
 '''
